73bf9d41c367e57001f936bcb81ccb88ce06f052
Raushan Kr singh <140451487+TheOneWhoCodess@users.noreply.github.com>
2026-01-07 03:15:12 +0530

    footer update

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-center py-2 text-gray-400">© 2025 Min Khant Kyaw</div>
+    <div className="text-center py-2 text-gray-400">© 2025 Raushan Kumar Singh</div>
   );
 };
 
